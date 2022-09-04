b043f71c36b93e2e7bba69ceab7c58386ec19a76
Amin <amin.haddou@frg.wwschool.de>
2022-09-04 18:48:07 +0200

    add command line argument parsing

--- src/main/java/us/duckul/lothar/Main.java
+++ src/main/java/us/duckul/lothar/Main.java
@@ -6,9 +6,23 @@ public class Main {
 
     public static void main(String[] args) {
         PrintStream printStream = System.out;
-
         Lothar lothar = Lothar.newInstance();
-        lothar.printName(printStream);
+
+
+
+        if (args.length == 0) {
+            printStream.println("Welcome to lothar-cli. Choose one of the following commands: \n" +
+                    "run");
+            return;
+        }
+
+        String operation = args[0];
+
+        if (operation.equals("run")) {
+            lothar.printName(printStream);
+        } else {
+            throw new UnsupportedOperationException("Operation %s not Supported".formatted(operation));
+        }
     }
 
 }
