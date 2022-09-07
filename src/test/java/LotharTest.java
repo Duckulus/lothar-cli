9683ad15ad14e3cc1c0fe6d341d74eabd7642398
Duckulus <76813487+Duckulus@users.noreply.github.com>
2022-09-07 07:31:54 +0000

    switch to junit assertions from java assert

--- src/test/java/LotharTest.java
+++ src/test/java/LotharTest.java
@@ -1,26 +1,30 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import us.duckul.lothar.Lothar;
 
-import java.util.Objects;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LotharTest {
 
     @Test
+    @DisplayName("Default Name of Lothar Class should be Lothar von Trotha")
     public void testDefaultFullName() {
         Lothar lothar = Lothar.LotharFactory.createLothar();
-        assert Objects.equals(lothar.getFullName(), "Lothar von Trotha");
+        assertEquals(lothar.getFullName(), "Lothar von Trotha");
     }
 
     @Test
+    @DisplayName("When instantiating an instance of Lothar with a String, its display name should equal it")
     public void testFullName() {
         String fullName = "Lothar";
         Lothar lothar = Lothar.LotharFactory.createLothar(fullName);
-        assert Objects.equals(lothar.getFullName(), fullName);
+        assertEquals(lothar.getFullName(), fullName);
     }
 
     @Test
+    @DisplayName("Sister name should be Clara")
     public void testSisterName() {
-        assert Lothar.getSisterName().equals("Clara");
+        assertEquals(Lothar.getSisterName(), "Clara");
     }
 
 }
